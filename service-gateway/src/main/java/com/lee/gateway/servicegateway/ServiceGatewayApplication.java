d12e303ed0a2f2ce4fb2704e5fa3ae903d667181
lishuai07 <17801093323@163.com>
2021-02-27 12:19:08 +0800

    dev修改

--- service-gateway/src/main/java/com/lee/gateway/servicegateway/ServiceGatewayApplication.java
+++ service-gateway/src/main/java/com/lee/gateway/servicegateway/ServiceGatewayApplication.java
@@ -8,6 +8,8 @@ public class ServiceGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceGatewayApplication.class, args);
+        System.out.println("Test分支修改内容");
+        System.out.println("dev修改内容");
     }
 
 }
